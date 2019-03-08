eb598b2e278300e4698a3a01465649a46643ed1f
Patrick Nymark <pn@ctdenmark.dk>
2019-03-08 22:47:09 +0100

    auth refactored to logic in controller

---
+++ controllers/auth/authController.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Model
+const User = require('../../models/User');
+
+exports.registerUser = (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    // Generate salt
+    bcrypt.genSalt(10, (err, salt) => {
+      // Hash password
+      bcrypt.hash(password, salt).then(hashedPassword => {
+        // Create new user
+        const newUser = new User({
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword
+        });
+
+        // Save new user to database
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => res.status(400).json(err));
+      });
+    });
+  });
+};
+
+exports.loginUser = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({ msg: 'Email does not exist' });
+      }
+
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            res.status(400).json({ msg: 'Password Incorrect' });
+          }
+
+          const payload = {
+            id: user._id,
+            name: user.firstName + ' ' + user.lastName
+          };
+
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 36000 },
+            (err, token) => {
+              res.json({ success: true, token: 'bearer ' + token });
+            }
+          );
+        })
+        .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err));
+};
--- routes/auth/users.js
+++ routes/auth/users.js
@@ -5,77 +5,27 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
-// Load Model
+// Controllers
+const authController = require('../../controllers/auth/authController');
+
+// Load Models
 const User = require('../../models/User');
 
+// @route   GET api/auth/
+// @desc    Get all users
+// @access  Public
 router.get('/', (req, res) => {
   User.find().then(users => res.json(users));
 });
 
-router.post('/register', (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+// @route   POST api/auth/register
+// @desc    Register new user route
+// @access  Public
+router.post('/register', authController.registerUser);
 
-  User.findOne({ email }).then(user => {
-    // Check if user exists
-    if (user) {
-      return res.status(400).json({ msg: 'User already exists' });
-    }
-
-    // Generate salt
-    bcrypt.genSalt(10, (err, salt) => {
-      // Hash password
-      bcrypt.hash(password, salt).then(hashedPassword => {
-        // Create new user
-        const newUser = new User({
-          firstName,
-          lastName,
-          email,
-          password: hashedPassword
-        });
-
-        // Save new user to database
-        newUser
-          .save()
-          .then(user => res.json(user))
-          .catch(err => res.status(400).json(err));
-      });
-    });
-  });
-});
-
-router.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return res.status(400).json({ msg: 'Email does not exist' });
-      }
-
-      bcrypt
-        .compare(password, user.password)
-        .then(isMatch => {
-          if (!isMatch) {
-            res.status(400).json({ msg: 'Password Incorrect' });
-          }
-
-          const payload = {
-            id: user._id,
-            name: user.firstName + ' ' + user.lastName
-          };
-
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            { expiresIn: 36000 },
-            (err, token) => {
-              res.json({ success: true, token: 'bearer ' + token });
-            }
-          );
-        })
-        .catch(err => res.status(400).json(err));
-    })
-    .catch(err => res.status(400).json(err));
-});
+// @route   POST api/auth/login
+// @desc    User login route
+// @access  Public
+router.post('/login', authController.loginUser);
 
 module.exports = router;
