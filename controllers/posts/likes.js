485b996dc1d606cafcfef9b6b1583b6652d6a5cb
Patrick Nymark <pn@ctdenmark.dk>
2019-03-12 13:07:08 +0100

    fixed some bugs

--- controllers/posts/likes.js
+++ controllers/posts/likes.js
@@ -9,28 +9,38 @@ const Post = require('../../models/Post');
 exports.likePost = (req, res) => {
   const { post_id } = req.params;
 
-  Post.findById(post_id).then(post => {
-    // No post found
-    if (!post) {
-      return res.status(400).json({ notfound: 'Post not found' });
-    }
+  Post.findById(post_id)
+    .then(post => {
+      // No post found
+      if (!post) {
+        return res.status(400).json({ notfound: 'Post not found' });
+      }
 
-    // Check if already liked
-    if (post.likes.map(like => like._id === req.user.id).length > 0) {
-      // Get remove index
-      const removeIndex = post.likes.map(like => like._id).indexOf(req.user.id);
+      // Check if already liked
+      if (post.likes.map(like => like._id === req.user.id).length > 0) {
+        // Get remove index
+        const removeIndex = post.likes
+          .map(like => like._id)
+          .indexOf(req.user.id);
 
-      // Remove like
-      post.likes.splice(removeIndex, 1);
+        // Remove like
+        post.likes.splice(removeIndex, 1);
+
+        // Save updated post
+        return post
+          .save()
+          .then(post => res.json(post))
+          .catch(err => res.status(500).json(err.message));
+      }
+
+      // Add to beginning of array
+      post.likes.unshift(req.user);
 
       // Save updated post
-      return post.save().then(post => res.json(post));
-    }
-
-    // Add to beginning of array
-    post.likes.unshift(req.user);
-
-    // Save updated post
-    post.save().then(post => res.json(post));
-  });
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(500).json(err.message));
+    })
+    .catch(err => res.status(500).json(err.message));
 };
