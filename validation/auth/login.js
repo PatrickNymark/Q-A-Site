ffaab03a3f73aebcf64162c7fd39d11479b83628
Patrick Nymark <pn@ctdenmark.dk>
2019-03-26 19:20:23 +0100

    small validation order fix

--- validation/auth/login.js
+++ validation/auth/login.js
@@ -3,19 +3,19 @@ const Validator = require('validator');
 // isEmpty
 const isEmpty = require('../isEmpty');
 
-module.exports = function(data) {
+module.exports = function (data) {
   const errors = {};
 
   // Check for undefined
   data.email = isEmpty(data.email) ? '' : data.email;
   data.password = isEmpty(data.password) ? '' : data.password;
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is not valid';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
   }
 
   if (Validator.isEmpty(data.password)) {
