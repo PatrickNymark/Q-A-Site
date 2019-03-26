805cead25317bc67857270adae484b0ae6fa3aa4
Patrick Nymark <pn@ctdenmark.dk>
2019-03-26 19:11:30 +0100

    fixed overriding error state

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,17 +1,11 @@
 import { GET_ERRORS } from '../actions/types';
 
-const initialState = {
-  errors: {}
-};
+const initialState = {};
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
-      return {
-        ...state,
-        errors: {}
-      };
-
+      return action.payload
     default:
       return state;
   }
