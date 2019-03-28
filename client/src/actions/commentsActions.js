266feece397cb94989fed1967b3eac0403248f6a
Patrick Nymark <pn@ctdenmark.dk>
2019-03-29 00:09:11 +0100

    added clear comments action

--- client/src/actions/commentsActions.js
+++ client/src/actions/commentsActions.js
@@ -31,3 +31,10 @@ export const getComments = (postID) => dispatch => {
     })
   })
 }
+
+export const clearComments = () => dispatch => {
+  dispatch({
+    type: GET_COMMENTS_SUCCESS,
+    payload: {}
+  })
+}
