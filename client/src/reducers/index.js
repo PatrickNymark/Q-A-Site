c63a847997b87d19499759d1b86f216de693bec5
Patrick Nymark <pn@ctdenmark.dk>
2019-04-01 09:51:44 +0200

    removed comments reducer and actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,11 +4,9 @@ import postReducer from './postReducer';
 import authReducer from './authReducer';
 import profileReducer from './profileReducer';
 import errorReducer from './errorReducer';
-import commentsReducer from './commentsReducer';
 
 export default combineReducers({
   post: postReducer,
-  comments: commentsReducer,
   auth: authReducer,
   profile: profileReducer,
   errors: errorReducer
