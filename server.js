4241223275a18ad44ebddc959ccc9906f5b175ed
Patrick Nymark <pn@ctdenmark.dk>
2019-05-02 15:47:35 +0200

    fixed serving of static assets for in production

--- server.js
+++ server.js
@@ -44,4 +44,14 @@ app.use('/api/auth', users);
 
 const port = 5000 || process.env.PORT;
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on ${port}`));
